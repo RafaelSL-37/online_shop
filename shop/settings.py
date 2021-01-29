5cae61a025705fec257775459696127f6cb5457c
Rafael SL <rafaelsilvadelima37@gmail.com>
2021-01-29 15:34:02 -0300

    Added pagination

--- shop/settings.py
+++ shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []#['loja-online-teste.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['loja-online-teste.herokuapp.com', 'localhost:8000']
 
 # Application definition
 
