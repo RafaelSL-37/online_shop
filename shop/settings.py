0e121006e24dfa4b43eaa833be4fc87361728c39
Rafael SL <59122617+RafaelSL-37@users.noreply.github.com>
2021-06-10 19:33:22 -0300

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-    #os.environ.get('SECRET_KEY')
-    #'kkepa517d2ku@1=bkqbt(*q6##tv67bd(&*hty%w86($*k-_fn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
